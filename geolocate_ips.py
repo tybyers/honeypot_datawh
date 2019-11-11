e6c2041a3b4e280bb07615e72a64b62260b81468
tybyers <tybyers@gmail.com>
2019-11-11 12:02:59 -0800

    wrote class to get ips from freegeoip.app

---
+++ geolocate_ips.py
@@ -0,0 +1,143 @@
+import requests
+import time
+import datetime
+
+class GeolocateIPs(object):
+    """ Using a list of IPs, retrieves IP geolocation information 
+    from the freegeoip.app API. We are limited to 15,000 calls per 
+    hour, so if a 403 message is returned, by default we pause for
+    5 minutes before trying again. If an output path is supplied,
+    the methods will scan that file for previously geolocated IPs,
+    so as not to repeat the geolocations, to save API calls. 
+
+    Parameters
+    ----------
+    ip_list_path: str, default None
+        Path to IP list data set. Module will expect one IP address per
+        line.
+
+    """
+
+    def __init__(self):
+
+        self.ip_set = set()
+        self.output_lines = []
+        self.failed_ips = [] # IPs that failed, for whatever reason
+
+    def _query_api(self, ip_addr):
+        """ Given an ip_addr as a string, query the freegeoip API and return the response
+
+        Parameters
+        ----------
+        ip_addr: str
+            IP address to query
+
+        Returns
+        -------
+        requests request
+        """
+
+        url_ip = "https://freegeoip.app/csv/{}".format(ip_addr)
+
+        headers = {
+            'accept': "application/csv",
+            'content-type': "application/csv"
+        }
+
+        response = requests.request("GET", url_ip, headers=headers)
+
+        return response
+
+    def load_ip_list(self, ip_list_path):
+        """  Load the IP list from a file. This function assumes the 
+        file is a list of IPs, one IP per line. It returns a set
+        of the IPs (i.e. removes duplicates). 
+
+        Parameters
+        ----------
+        ip_list_path: str
+            Path to IP list.
+
+        Returns
+        -------
+        set
+            A set of the IP list (removes duplicates)
+        """
+
+        with open(ip_list_path, 'r') as ipf:
+            ips = ipf.readlines()
+            ips = [ip.strip() for ip in ips] # remove newlines
+
+        self.ip_set = set(ips)
+        print('Found {} unique IP addresses.'.format(len(self.ip_set)))
+        return self.ip_set
+
+    def geo_ips(self, ip_set, output_path, write_interval=5000, sleep_interval=300):
+
+        self.output_lines = []
+        # write to file every 5000 geolocated IPs
+        prev_write_record = 0
+        record_count = 0
+
+        # first find unique IPs
+        with open(output_path, 'r') as geod:
+            gathered = [g.split(',')[0] for g in geod]
+            gathered = set(gathered)
+        print('Previously geolocated {} IP addresses.'.format(len(gathered)))
+
+        # make sure `ip_set` is the right type:
+        if not isinstance(ip_set, set):
+            # try to force ip_set to be a set
+            if isinstance(ip_set, list):
+                ip_set = set(ip_set)
+            else:
+                raise TypeError('`ip_set` parameter must be of type set or list.')
+
+        need_geos = ip_set.difference(gathered)
+
+        time_needed = round(len(need_geos) / 15000, 1)
+        msg = (len(need_geos), time_needed)
+        print('Fetching {} new geolocations. This will take at least {} hours.'.format(msg[0], msg[1]))
+
+
+        for ip in need_geos:
+            record_count += 1
+            response = self._query_api(ip)
+            if response.status_code == 200:
+                newline = ','.join((ip, response.text))
+                self.output_lines.append(newline)
+            elif response.status_code == 403:
+                # print a sleep message
+                print('{}: Reached hourly query limit. Sleeping for {} seconds.'.\
+                    format(datetime.datetime.now(), sleep_interval))
+                time.sleep(sleep_interval)
+            else:
+                print('{}: Return code: {} for IP {}. Adding IP to `failed_ips` list.'.\
+                    format(datetime.datetime.now(), response.status_code, ip))
+                self.failed_ips.append(ip)
+        
+            # at the write interval, write file
+            if record_count % write_interval == 0:
+                print('{}: Writing IP geolocations for IPs {}-{} to file.'.\
+                    format(datetime.datetime.now(), prev_write_record, record_count))
+                with open(output_path, 'a') as outfile:
+                    outfile.writelines(self.output_lines)
+                self.output_lines = [] # reset output lines
+                prev_write_record = record_count
+
+        # write at the end too
+        print('{}: Writing IP geolocations for IPs {}-{} to file.'.\
+            format(datetime.datetime.now(), prev_write_record, record_count))
+        with open(output_path, 'a') as outfile:
+            outfile.writelines(self.output_lines)
+
+        if len(self.failed_ips) > 0:
+            print('Failed to write {} IPs. See `failed_ips` attribute of object.'.\
+                    format(len(self.failed_ips)))
+
+        
+
+
+
+
+
