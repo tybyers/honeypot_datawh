c92d7d7a2bcf0290ff193afa68611e99c4cdafd1
tybyers <tybyers@gmail.com>
2019-11-18 11:34:08 -0800

    script for deleting the cluster

---
+++ cluster_delete.py
@@ -0,0 +1,44 @@
+# this script deletes the redshift cluster if it is available
+
+from redshift import redshift
+CONFIG_FILENAME = './aws.cfg'  # note: not included in GH repo for privacy. See `aws_example.cfg` for example
+
+def check_available(rs):
+    """ Given a redshift object, determine if the cluster is available. 
+
+    Parameters
+    ----------
+    rs: redshift.redshift object
+    
+    Returns
+    -------
+    bool; `True` if cluster is available, `False` if being created. Throws an error if other status found.
+
+    """
+    info = rs.get_cluster_info()
+    if info['ClusterStatus'] == 'available':
+        return True
+    elif info['ClusterStatus'] == 'deleting':
+        raise AttributeError(f'Cluster is currently deleting. Please wait and try again.\n{info}')
+    elif info['ClusterStatus'] == 'creating': 
+        return False
+    
+    raise NameError(f'Could not get cluster status information. Current information about the cluster: \n{info}')
+
+def main():
+    """ Attempts to delete the redshift cluster. If it cannot, it will notify the user and ask
+    them to try again. 
+    """
+    rs = redshift(config_file=CONFIG_FILENAME)
+    
+    # check if cluster already available
+    clust_avail = check_available(rs)
+
+    if clust_avail:
+        rs.shutdown_cluster()
+        print(f'Shutting down cluster. Cluster info: \n{rs.get_cluster_info()}')
+    else:
+        print(f'Cannot shut down cluster in current state. Please try again. Cluster info: \n{rs.get_cluster_info}')
+
+if __name__ == '__main__':
+    main()
